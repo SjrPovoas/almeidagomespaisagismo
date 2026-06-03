c7a5bfe5029cbffac2195cdafc16a33dc2dda411
SjrPovoas <87617524+SjrPovoas@users.noreply.github.com>
2026-06-02 21:46:57 -0300

    Autenticação Básica (Basic Auth) COM Password Protection utilizando Edge Middleware

--- middleware.js
+++ middleware.js
@@ -1,20 +1,24 @@
-import { NextResponse } from 'next/server';
+export const config = {
+  matcher: '/admin.html',
+};
 
-export default function middleware(request) {
+export default async function middleware(request) {
   const basicAuth = request.headers.get('authorization');
   const user = process.env.ADMIN_USER;
   const password = process.env.ADMIN_PASS;
 
   if (basicAuth) {
     const authValue = basicAuth.split(' ')[1];
+    // Decodifica o base64
     const [decodedUser, decodedPassword] = atob(authValue).split(':');
 
     if (decodedUser === user && decodedPassword === password) {
-      // ESTA É A CORREÇÃO:
-      return NextResponse.next(); 
+      // Retorna 'null' para deixar o request original (o admin.html) passar
+      return new Response(null); 
     }
   }
 
+  // Se a senha falhar ou não existir, pede autenticação
   return new Response('Auth Required', {
     status: 401,
     headers: {
